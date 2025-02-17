ab1aae38f67ce2d982dfd6abb824530aab0c6f2e
urieldelnegro <urieldelnegro@gmail.com>
2025-02-16 21:24:39 -0300

    se deja el path del controller normalizado

--- src/main/java/com/parking/app/BookingService/controller/BookingController.java
+++ src/main/java/com/parking/app/BookingService/controller/BookingController.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/abm-reservas")
+@RequestMapping(value = "/booking")
 public class BookingController {
 
     @Autowired
